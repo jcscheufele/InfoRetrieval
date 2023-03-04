b32b1a2bb9f48ca12a40a321bd92668cdf849378
jcscheufele <57052259+jcscheufele@users.noreply.github.com>
2023-03-03 23:55:18 -0500

    completed

--- hw3/hw3_joseph_scheufele.py
+++ hw3/hw3_joseph_scheufele.py
@@ -10,6 +10,7 @@
 # first, create a student object
 # ########################################
 
+import re
 import cs547
 MY_NAME = "Joseph Scheufele"
 MY_ANUM  = 361589387 # put your UID here
@@ -37,8 +38,9 @@ student = cs547.Student(
 # now, write some code
 # ########################################
 
-import bs4 as BeautifulSoup  # you will want this for parsing html documents
-
+from bs4 import BeautifulSoup as bs  # you will want this for parsing html documents
+from urllib.request import urlopen
+import numpy as np
 
 # our index class definition will hold all logic necessary to create and search
 # an index created from a web directory
@@ -53,7 +55,20 @@ class PageRankIndex(object):
     def __init__(self):
         # you'll want to create something here to hold your index, and other
         # necessary data members
-        pass
+        self.root = ""
+        self.web = {}
+        self.index = {}
+    
+    
+    def _crawler(self, root):
+        soup = bs(urlopen(root), 'html.parser')
+        anchors = soup.find_all('a')
+        linkNums = []
+        for anchor in anchors:
+            number = int(str(anchor.string).split(".")[0].split("_")[1])
+            linkNums.append(number)
+        return linkNums
+        
 
     # index_url( url )
     # purpose: crawl through a web directory of html files and generate an
@@ -64,8 +79,67 @@ class PageRankIndex(object):
     # parameters:
     #   url - a string containing a url to begin indexing at
     def index_url(self, url):
+        self.root = url.replace("index.html", "")
+        #print(f"Root: {self.root}")
+        soup = bs(urlopen(url), 'html.parser')
+        anchors = soup.find_all('a')
+        for anchor in anchors:
+            link = str(anchor.get('href'))
+            #print(f"Link: {link}")
+            if link not in self.web.keys():
+                self.web[link] = [self._crawler(self.root+link), 0]
         # ADD CODE HERE
-        return 0
+        #print(self.web)
+
+        p = np.zeros((len(anchors), len(anchors)))
+        t = np.ones((len(anchors), len(anchors))) * .1
+
+        for i in range(len(anchors)):
+            links = self.web[f"d_{i}.html"][0]
+            for link in links:
+                #print("LINK", link)
+                #print(p[i, link])
+                p[i, link] = 1/len(links)
+
+        #print(p)
+
+        P = .9*p + .1*t
+
+        #print(P)
+
+        err = 1000
+        xPrev = np.zeros(len(anchors))
+        xPrev[0] = 1
+        iteration = 0
+        while err > 1e-10/len(anchors):
+            #print("Iteration: ", iteration)
+            #print("xPrev: ", xPrev)
+            xNext = xPrev.dot(P)
+            #print("xNext: ", xNext)
+            err = np.sum(np.subtract(xNext, xPrev)**2)
+            #print("Error: ", err)
+            iteration += 1
+            xPrev = xNext
+
+        for i in range(len(anchors)):
+            self.web[f'd_{i}.html'][1] = xPrev[i]
+            
+        #print(self.web)
+
+        for key in self.web.keys():
+            number = int(key.split(".")[0].split("_")[1])
+            soup = bs(urlopen(self.root+key), 'html.parser')
+            text = soup.get_text()
+            tokens = self.tokenize(text)
+            for token in tokens:
+                if token not in self.index.keys():
+                    self.index[token] = [number]
+                elif number not in self.index[token]:
+                    self.index[token].append(number)
+
+        #print(self.index)
+
+        return len(anchors)
 
     # tokenize( text )
     # purpose: convert a string of terms into a list of terms 
@@ -74,8 +148,10 @@ class PageRankIndex(object):
     # parameters:
     #   text - a string of terms
     def tokenize(self, text):
-        # ADD CODE HERE
-        return []
+        tokens = []
+        tokens = re.sub('[^0-9a-zA-Z]+', ' ', text.lower())
+        #print(tokens, tokens.split())
+        return tokens.split()
 
     # ranked_search( text )
     # purpose: searches for the terms in "text" in our index and returns
@@ -86,8 +162,25 @@ class PageRankIndex(object):
     # parameters:
     #   text - a string of query terms
     def ranked_search(self, text):
+        tokens = self.tokenize(text)
+        docs = []
+
+        for token in tokens:
+            docs.append(set(self.index[token]))
+            
         # ADD CODE HERE
-        return []
+        
+        results = []
+        finalDocs = set.intersection(*docs)
+        #print("Results: ", finalDocs)
+
+        for doc in finalDocs:
+            score = self.web[f"d_{doc}.html"][1]
+            results.append((self.root+f"d_{doc}.html", score))
+
+        results.sort(key = lambda x: x[1], reverse=True)
+
+        return results[:10]
 
 
 # now, we'll define our main function which actually starts the indexer and
